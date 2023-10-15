02718800e86ccc0fcaf829b86eeac5eedce7f63e
Ahmed hassen <aahhmmeeddhhaasseenn12332@gmail.com>
2023-10-15 16:34:08 +0300

    15/10#4

--- src/routers/APIs/V1/download.ts
+++ src/routers/APIs/V1/download.ts
@@ -5,108 +5,36 @@ import puppeteer from 'puppeteer'
 import { exec } from 'node:child_process'
 import { promises } from 'fs'
 import { promisify } from 'node:util'
+import getDown from '../../../middlewaer/getDown'
 var https = require('https')
 const router = express.Router()
 const root = process.cwd()
 var config = require(root + '/config.json')
 
 router.get('/:id', async (req, res) => {
-    const { stdout: chromiumPath } = await promisify(exec)("which chromium");
-
-    const browser = await puppeteer.launch({
-        headless: false,
-        userDataDir: './tmp',
-        waitForInitialPage: true,
-      executablePath: chromiumPath.trim(),
-        timeout: 0,
-        args: [
-        //    '--proxy-server=192.168.1.9:44355',
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-        ],
-    })
-  
-   
-    const page = await browser.pages().then((pages) => pages[0])
-    await page.goto(config.url + '/download/' + req.params.id + '/56151')
-    const html = await page.content()
-    const title = await page.title()
-    if (!title.startsWith('تحق')) {
-        var $ = cheerio.load(html)
-        var rss = {
-            link: $('.link.btn.btn-light').attr('href'),
-            name: $('a[download]').last().text().split('.AKWAM.')[0],
-        }
-        const response = await axios.get(rss.link!, {
-            responseType: 'stream',
-            httpsAgent: new https.Agent({
-                rejectUnauthorized: false,
-            }),
-        })
-        const stream = response.data
-        res.writeHead(200, {
-            'Content-Type': 'application/octet-stream',
-            'content-name': rss.name + '.mp4',
-            'Content-Disposition': `attachment; filename=${rss.name}.mp4`,
-            'content-length': response.headers['content-length'],
-        })
-        stream.on('data', (data: any) => {
-            res.write(data)
-        })
-    
-        stream.on('end', () => {
-            res.end()
-            console.log('stream done')
-        })
-    } else {
-        res.json({
+   getDown(req.params.id).then((data) => {
+        res.redirect(data.link,200)
+    }).catch((err) => {
+        res.send({
             status: false,
-            msg: 'app need verification',
+            msg: err.message,
         })
-    
-    }
+    })
 })
 
 router.get('/:id/info', async (req, res) => {
-    // config.url + '/download/' + req.params.id + '/56151',
-   const { stdout: chromiumPath } = await promisify(exec)("which chromium");
-
-    const browser = await puppeteer.launch({
-        headless: false,
-        userDataDir: './tmp',
-        waitForInitialPage: true,
-      executablePath: chromiumPath.trim(),
-        timeout: 0,
-        args: [
-          //  '--proxy-server=192.168.1.9:44355',
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-        ],
-    })
-  
-   
-    const page = await browser.pages().then((pages) => pages[0])
-    await page.goto(config.url + '/download/' + req.params.id + '/56151')
-    const html = await page.content()
-    const title = await page.title()
-    if (!title.startsWith('تحق')) {
-        const $ = cheerio.load(html)
-        var rss = {
-            link: $('.link.btn.btn-light').attr('href'),
-            name: $('a[download]').last().text().split('.AKWAM.')[0],
-        }
-        await browser.close()
-        res.json({
-            name: rss.name,
-            link: rss.link,
-            size: 0,
+    getDown(req.params.id).then((data) => {
+        res.send({
+            name: data.name,
+            link: data.link,
         })
-    } else {
-        res.json({
+        
+    }).catch((err) => {
+        res.send({
             status: false,
-            msg: 'app need verification',
+            msg: err.message,
         })
-    }
+    })
 })
 
 module.exports = router
--- src/routers/APIs/V1/watch.ts
+++ src/routers/APIs/V1/watch.ts
@@ -8,72 +8,19 @@ import { promisify } from 'node:util'
 import contentDisposition from 'content-disposition'
 import https from 'https'
 import puppeteer from 'puppeteer'
+import getDown from '../../../middlewaer/getDown'
 const root = process.cwd()
 var config = require(root + '/config.json')
 router.get('/:id', async (req: Request, res: Response) => {
-    const { stdout: chromiumPath } = await promisify(exec)("which chromium");
-
-    const browser = await puppeteer.launch({
-        headless: false,
-        userDataDir: './tmp',
-        waitForInitialPage: true,
-      executablePath: chromiumPath.trim(),
-        timeout: 0,
-        args: [
-           // '--proxy-server=192.168.1.9:44355',
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-        ],
-    })
-  
-   
-    const page = await browser.pages().then((pages) => pages[0])
-    await page.goto(config.url + '/download/' + req.params.id + '/56151')
-    const html = await page.content()
-    const title = await page.title()
-    if (!title.startsWith('تحق')) {
-        const $ = cheerio.load(html)
-        var rss = {
-            link: $('.link.btn.btn-light').attr('href'),
-            name: $('meta')
-                .filter(function (i: any, el: any) {
-                    return $(el).attr('property') === 'og:title'
-                })
-                .last()
-                .attr('content'),
-        }
-        const response = await axios.get(rss.link!, {
-            responseType: 'stream',
-            headers: {
-                Range: req.headers.range ?? 0,
-            },
-            httpsAgent: new https.Agent({
-                rejectUnauthorized: false,
-            }),
-        })
-        response.headers['Content-Type'] = 'video/mp4'
-        response.headers['Content-name'] = contentDisposition(rss.name)
-        ;(response.headers['Content-Disposition'] = contentDisposition(rss.name)),
-            res.writeHead(206, response.headers)
-        response.data.on('data', (data: any) => {
-            res.write(data)
-        })
-        console.log(rss.name)
-    
-        response.data.on('end', () => {
-            console.log('stream done')
-            res.end()
-        })
-        response.data.on('error', (err: any) => {
-            console.log(err)
-        })
-    } else {
+    getDown(req.params.id).then((data) => {
+        res.redirect(data.link, 200)
+    }).catch((err) => {
         res.send({
             status: false,
-            message: 'app need  to verify',
+            msg: err.message,
         })
-  
     }
+    )
 })
 
 module.exports = router
