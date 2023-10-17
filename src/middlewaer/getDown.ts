622ea00ebcaa957090e256055c432adca2b5e71c
Ahmed hassen <aahhmmeeddhhaasseenn12332@gmail.com>
2023-10-18 02:45:37 +0300

    18/10#3

--- src/middlewaer/getDown.ts
+++ src/middlewaer/getDown.ts
@@ -1,73 +1,19 @@
-import { default as axios } from 'axios'
-import cheerio, { load } from 'cheerio'
-import express from 'express'
-import puppeteer from 'puppeteer'
-import { exec } from 'node:child_process'
-import fs, { promises } from 'fs'
-import { promisify } from 'node:util'
-var https = require('https')
-const router = express.Router()
+import axios from 'axios'
 const root = process.cwd()
 var config = require(root + '/config.json')
 
-var conn = ''
 export default async function (id: string): Promise<{
     name: string
     link: string
 }> {
-
-  //  const { stdout: chromiumPath } = await promisify(exec)('which chromium')
-
-    const browser = await puppeteer
-        .connect({ browserWSEndpoint: conn })
-        .then((browser) => browser)
-        .catch(async (err) => {
-            console.log(err)
-            const browser = await puppeteer.launch({
-                headless: false,
-              //  userDataDir: './tmp',
-        //        executablePath: chromiumPath,
-                waitForInitialPage: true,
-                timeout: 0,
-
-                args: [
-                    '--proxy-server=192.168.1.9:44355',
-                    '--no-sandbox',
-                    '--disable-setuid-sandbox',
-                ],
+    return new Promise(async (resolve, reject) => {
+        axios
+            .get(config.download + id, {})
+            .then(async (res) => {
+                resolve(res.data)
             })
-            conn = browser.wsEndpoint()
-            console.log(conn)
-            return browser
-        })
-
-    const page = await browser.pages().then((pages) => pages[0])
-    const cookiesBuffer = await fs.promises.readFile(root + '/cookies.json')
-    const cookies = JSON.parse(cookiesBuffer.toString())
-    await page.setCookie(...cookies)
-    await page.goto(config.url + '/download/' + id + '/56151')
-    const html = await page.content()
-    const title = await page.title()
-    if (!title.startsWith('تحق')) {
-        const $ = cheerio.load(html)
-        var rss = {
-            link: $('.link.btn.btn-light').attr('href'),
-            name: $('a[download]').last().text().split('.AKWAM.')[0],
-        }
-         const cookies = await page.cookies()
-         console.log(cookies)
-        // fs.writeFileSync(
-        //     root + '/cookies.json',
-        //     JSON.stringify(cookies, null, 2)
-        // )
-        
-        browser.disconnect()
-        return {
-            name: rss.name,
-            link: rss.link!,
-        }
-
-    }  else {
-        throw new Error('App Need to get Verify')
-    }
+            .catch((err) => {
+                reject(err)
+            })
+    })
 }
--- src/routers/APIs/V1/watch.ts
+++ src/routers/APIs/V1/watch.ts
@@ -1,26 +1,30 @@
-import { default as axios } from 'axios'
-import cheerio from 'cheerio'
+import { AxiosRequestHeaders, default as axios } from 'axios'
 import express, { Request, Response } from 'express'
 const router = express.Router()
-import { exec } from 'node:child_process'
-import { promises } from 'fs'
-import { promisify } from 'node:util'
 import contentDisposition from 'content-disposition'
 import https from 'https'
-import puppeteer from 'puppeteer'
 import getDown from '../../../middlewaer/getDown'
 const root = process.cwd()
 var config = require(root + '/config.json')
 router.get('/:id', async (req: Request, res: Response) => {
-    getDown(req.params.id).then((data) => {
-        res.redirect(data.link, 200)
-    }).catch((err) => {
-        res.send({
-            status: false,
-            msg: err.message,
+    getDown(req.params.id)
+        .then((data) => {
+            var vid = axios({
+                method: 'get',
+                url: data.link,
+                responseType: 'stream',
+                httpsAgent: new https.Agent({
+                    rejectUnauthorized: false,
+                }),
+                
+            })
         })
-    }
-    )
+        .catch((err) => {
+            res.send({
+                status: false,
+                msg: err.message,
+            })
+        })
 })
 
 module.exports = router
